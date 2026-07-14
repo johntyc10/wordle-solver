7f21258fe0870a9c709226f42c3c64923b49b8d3
johntyc10 <johnchan070507@gmail.com>
2026-07-15 01:04:39 +0800

    Honestly i forgot what i did, some rewrites? More statistics

--- solver_benchmark.py
+++ solver_benchmark.py
@@ -3,7 +3,7 @@ import random
 import time
 import copy
 from wordle_solver import BaseWordle, log
-from datetime import datetime
+from datetime import datetime, timedelta
 from pathlib import Path
 import json
 
@@ -12,23 +12,22 @@ def avg(x: Iterable):
         return 0
     return sum(x) / len(x)
 
-class WordleSimulator(BaseWordle):
+class WordleSolverBenchmark(BaseWordle):
     def __init__(self, use_tqdm = False):
         super().__init__(use_tqdm)
 
         self.all_words_persistence: List[str] = self._load_words(self.all_words_path)
         self.possible_words_persistence: Set[str] = set(self._load_words(self.possible_words_path))
+        self.possible_words_list = self._load_words(self.possible_words_path)
 
     def reset(self):
         self.all_words = copy.deepcopy(self.all_words_persistence)
         self.possible_words = copy.deepcopy(self.possible_words_persistence)
         self.guess_history: List[Tuple[str, str]] = []
-        # log(f"{len(self.all_words) = }")
-        # log(f"{len(self.possible_words) = }")
 
-    def save_guess_histories(self):
-        filename = f"simulator_guess_histories_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.json"
-        dir = Path("./output/")
+    def save_statistics(self):
+        filename = f"benchmark_guess_histories.json"
+        dir = Path(f"./output/{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}")
         dir.mkdir(parents=True, exist_ok=True)
 
         with open(dir / filename, "w") as f:
@@ -44,10 +43,16 @@ class WordleSimulator(BaseWordle):
         for i in range(1, 7):
             log(f"{i}: {guesses_taken.count(i)}")
 
-        log(f"Average guesses taken: {avg(guesses_taken):.3f}")
-        log(f"Average time taken per game: {avg(self.time_taken):.3f}s")
-        log(f"Average time taken per round: {sum(self.time_taken) / sum(guesses_taken):.3f}s")
-        log(f"Success rate: {len(guesses_taken) / self.games_played * 100:.3f}% ({len(guesses_taken)}/{self.games_played})")  # couldnt care less about ZeroDivisionError here
+        log(f"Average guesses taken: {avg(guesses_taken)}")
+        log(f"Average time taken per game: {avg(self.time_taken)}s")
+        log(f"Average time taken per round: {sum(self.time_taken) / sum(guesses_taken)}s")
+        log(f"Success rate: {len(guesses_taken) / self.games_played * 100}% ({len(guesses_taken)}/{self.games_played})")  # couldnt care less about ZeroDivisionError here
+
+        log()
+        log(f"Start time: {datetime.fromtimestamp(self.start_time).strftime("%Y/%m/%d, %H:%M:%S")}")
+        log(f"End time: {datetime.now().strftime("%Y/%m/%d, %H:%M:%S")}")
+        log(f"Time taken: {timedelta(seconds=time.time() - self.start_time)}")
+
 
     def play_one_game(self, secret: str) -> bool:
         """
@@ -87,32 +92,36 @@ class WordleSimulator(BaseWordle):
 
         return success
 
-    def simulate(self, n=100, first_opener="TARES"):
-        log("===== SIMULATION START =====")
+    def run(self, first_opener="TARES"):
+        log("===== BENCHMARK START =====")
         log("Press CTRL+C to terminate")
         try:
+            self.start_time = time.time()
+
             self.first_opener = first_opener
             self.guess_histories = []
             self.time_taken = []
             self.games_played = 0
+
+            n = len(self.possible_words_list)
             for i in range(n):
                 self.reset()
 
-                secret = random.choice(list(self.possible_words))
+                secret = self.possible_words_list[i]
                 log(f"-> GAME {i}/{n} (secret: {secret})")
 
-                timestamp = time.time_ns()
+                timestamp = time.time()
                 success = self.play_one_game(secret)
                 if success:
                     self.guess_histories.append(self.guess_history)
-                    self.time_taken.append((time.time_ns() - timestamp) * 1e-9)
+                    self.time_taken.append(time.time() - timestamp)
                 self.games_played += 1
         except KeyboardInterrupt:
             pass  # intended way of terminating
         finally:  # potential race conditions on every variable but i dont care
             self.print_statistics()
-            self.save_guess_histories()
+            self.save_statistics()
 
 if __name__ == "__main__":
-    sim = WordleSimulator()
-    sim.simulate(n=1000, first_opener="TARES")  # TODO: accept arguments in command line
+    benchmark = WordleSolverBenchmark()
+    benchmark.run(first_opener="TARES")  # TODO: accept arguments in command line
